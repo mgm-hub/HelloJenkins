0186a63c5b01cbed21dcdb4b9ddb40a0a05fd055
mgm-hub <thisismywebportal@gmail.com>
2016-06-27 13:04:07 -0700

    New Commit

---
+++ src/MainController/RunCore/MainRun.java
@@ -0,0 +1,14 @@
+package MainController.RunCore;
+
+/**
+ * Created by mmoscatello on 6/27/2016.
+ */
+public class MainRun {
+
+    public static void main(String[] args) throws Exception {
+
+        System.out.print("Hello Jenkins");
+    }
+
+
+}
